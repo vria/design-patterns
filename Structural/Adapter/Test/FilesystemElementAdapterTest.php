869331d8851dcc6363b9327fb99f75fead3bb49f
Vladyslav Riabchenko <vriabchenko@webnet.local>
2019-06-01 00:52:06 +0200

    correct test

--- Structural/Adapter/Test/FilesystemElementAdapterTest.php
+++ Structural/Adapter/Test/FilesystemElementAdapterTest.php
@@ -16,14 +16,7 @@ class FilesystemElementAdapterTest extends \PHPUnit_Framework_TestCase
         $node = new FilesystemAdapter(__DIR__.'/filesystem_adapter_fixtures');
         $rendered = $renderer->render($node);
 
-        $expected = "filesystem_adapter_fixtures\n"
-                    ."--1_one\n"
-                    ."----1_duo\n"
-                    ."------1_trois.txt\n"
-                    ."------2_trois.txt\n"
-                    ."----2_duo\n"
-                    ."--2_one\n"
-                    ."--3_one.txt\n";
+        $expected = "test string";
 
         $this->assertEquals($expected, $rendered);
     }
